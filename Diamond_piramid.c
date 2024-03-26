a787661837cbf3bf6da724b062cda0b55bd17eb2
Md.MishkaT_uL Bary Sifat <144310771+Sifat049@users.noreply.github.com>
2024-03-26 14:17:13 +0600

    Piramid Pattern

---
+++ Diamond_piramid.c
@@ -0,0 +1,64 @@
+//diamond piramid
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int num,row,col;
+    printf("Enter Num : ");
+    scanf("%d",&num);
+      for(row=1; row<=num; row++)
+
+    {
+        for(col=1; col<=(num)-row; col++)
+        {
+            printf(" ");
+        }
+        for(col=1; col<=row; col++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+      for(row=num-1; row>=1; row--)
+
+    {
+        for(col=1; col<=(num)-row; col++)
+        {
+            printf(" ");
+        }
+        for(col=1; col<=row; col++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+   /* for(row=1; row<=num; row++)
+
+    {
+        for(col=1; col<=(num)-row; col++)
+        {
+            printf(" ");
+        }
+        for(col=1; col<=2*row-1; col++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+      for(row=num-1; row>=1; row--)
+
+    {
+        for(col=1; col<=(num)-row; col++)
+        {
+            printf(" ");
+        }
+        for(col=1; col<=2*row-1; col++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+    return 0;
+}
